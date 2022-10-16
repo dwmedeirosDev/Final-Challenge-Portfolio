5cff33b828c7c334e3b630023ed4e40fd63c34bc
unknown <davidmedeiros94@hotmail.com>
2022-10-16 20:57:49 -0300

    OK

--- src/Components/Home/Index.jsx
+++ src/Components/Home/Index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import * as S from './Style'
 import Header from '../../Services/Header/Index'
+import Linkedin from '../../Assets/Social-Media/linkedin.png'
+import Whatsapp from '../../Assets/Social-Media/whatsapp.png'
+import GitHub from '../../Assets/Social-Media/github.png'
+import Photo5 from '../../Assets/Photos/photo5.png'
 
 export default function Home(){
     return(
-        <>
+        <S.ContainerHome>
             <Header />
-        </>
+            <S.All>
+                <S.Content>
+                    <S.Text>
+                        <S.H1>Bem vindo ao meu Portfólio</S.H1>
+                        <S.H3>Minhas Redes Sociais:</S.H3>
+                        <S.SMBox>
+                            <a href='https://www.linkedin.com/in/dwmedeiros/' target='_blank'><S.SM src={Linkedin} alt='LinkedIn'/></a>
+                            <a href='https://wa.me/5521982916500' target='_blank'><S.SM src={Whatsapp} alt='Whatsapp'/></a>
+                            <a href='https://github.com/dwmedeirosDev' target='_blank'><S.SM src={GitHub} alt='GitHub'/></a>  
+                        </S.SMBox>
+                    </S.Text>
+                </S.Content>
+                <S.Box>
+                    <S.Photo src={Photo5} alt='Minha foto - David'/>
+                </S.Box>
+            </S.All>
+        </S.ContainerHome>
     )
 }
--- src/Components/Home/Style.js
+++ src/Components/Home/Style.js
@@ -1,1 +1,97 @@
 import styled from 'styled-components'
+import Background from '../../Assets/Background/background.jpg'
+
+export const ContainerHome = styled.section`
+    background-image: url(${Background});
+    font-family: 'Ubuntu', sans-serif;
+`
+
+export const All = styled.div`
+    width: 100%;
+    height: 79vh;
+    display: flex;
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        flex-direction: column;
+        height: 95vh;
+    }
+`
+
+export const Content = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        align-items: center;
+        padding-top: 10%;
+    }
+`
+
+export const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const H1 = styled.h1`
+    color: white;
+    font-size: 1.8rem;
+    width: 100%;
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const H3 = styled.h1`
+    color: white;
+    font-weight: lighter;
+    width: 100%;
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const SMBox = styled.div`
+    width: 250px;
+    height: 10vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+export const Box = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        justify-content: center;
+    }
+`
+
+export const SM = styled.img`
+    width: 50px;
+    transition: all 0.1s;
+
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const Photo = styled.img`
+    width: 300px;
+    border-radius: 10px;
+    margin-left: 10%;
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        margin-left: 0;
+        padding-top: 10%;
+        border-radius: 10px;
+    }
+`
