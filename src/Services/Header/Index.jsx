c8add572adc9d5e6645de1e7b61bdaad0ce74aad
unknown <davidmedeiros94@hotmail.com>
2022-10-16 19:37:24 -0300

    OK

--- src/Services/Header/Index.jsx
+++ src/Services/Header/Index.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import * as S from './Style'
+
 
 export default function Header(){
     return(
-        <section>
-            <div>
-                <h1>Portfólio</h1>
-            </div>
-            <div>
-                <Link to='/'>Home</Link>
-                <Link to='/sobremim'>Quem Sou Eu</Link>
-                <Link to='/projetos'>Projetos</Link>
-                <Link to='/obs'>Obs</Link>
-            </div>
-        </section>
+        <S.ContainerHeader>
+            <S.Title>
+                <S.H1>Portfólio Web</S.H1>
+            </S.Title>
+            <S.Menu>
+                <S.SLink to='/'>Home</S.SLink>
+                <S.SLink to='/sobremim'>Quem Sou Eu</S.SLink>
+                <S.SLink to='/projetos'>Projetos</S.SLink>
+                <S.SLink to='/obs'>Obs</S.SLink>
+            </S.Menu>
+        </S.ContainerHeader>
     )
 }
---
+++ src/Services/Header/Style.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import Background from '../../Assets/Background/background.jpg'
+
+export const ContainerHeader = styled.section`
+    width: 100%;
+    height: 15vh;
+    display: flex;
+    justify-content: space-between;
+    background-image: url(${Background});
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        flex-direction: column;
+        align-items: center;
+        height: 20vh;
+        justify-content: space-evenly;
+    }
+`
+
+export const Title = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 7%;
+    color: #f3f3f3;
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        margin-left: 0;
+        width: 100%;
+        justify-content: center;
+    }
+`
+
+export const H1 = styled.h1`
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 2rem;
+`
+
+export const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 60%;
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        width: 100%;
+        justify-content: center;
+    }
+`
+
+export const SLink = styled(Link)`
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 1.2rem;
+    text-decoration: none;
+    padding: 10px;
+    margin-right: 5%;
+    color: white;
+    position: relative;
+    text-decoration: none;
+
+    &:before {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 4px;
+        border-radius: 4px;
+        background-color: #f3f3f3;
+        bottom: 0;
+        left: 0;
+        transform-origin: right;
+        transform: scaleX(0);
+        transition: transform .3s ease-in-out;
+    }
+
+    &:hover:before {
+        transform-origin: left;
+        transform: scaleX(1);
+    }
+
+    @media only screen and (min-width: 375px) and (max-width: 815px){
+        margin-right: 0;
+    }
+`
