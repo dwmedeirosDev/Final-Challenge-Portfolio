d0e859ebbf8fb0430c7dd82653acace0ae1cbdfb
unknown <davidmedeiros94@hotmail.com>
2022-10-18 10:11:59 -0300

    OK

--- src/Components/Projects/Index.jsx
+++ src/Components/Projects/Index.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import * as S from './Style'
+import Portflow from '../../Assets/Projects/portflow.png'
+import DogAPI from '../../Assets/Projects/dogapi.png'
+import Todolist from '../../Assets/Projects/todolist.png'
+import Header from '../../Services//Header/Index'
+import Footer from '../../Services/Footer/Index'
 
 export default function Projects(){
     return(
-        <h1>Projects Page</h1>
+        <S.ContainerProjects>
+            <Header />
+            <S.All>
+                <S.Project>
+                    <S.Img src={Portflow} alt='Portflow' />
+                    <S.Content>
+                        <S.H2>Portflow</S.H2>
+                        <S.P><S.A href="https://dwmedeirosdev.github.io/Portflow-Project/" target="_blank" rel="noreferrer">Site</S.A></S.P>
+                        <S.P><S.A href="https://github.com/dwmedeirosDev/Portflow-Project" target="_blank" rel="noreferrer">Repositório GitHub</S.A></S.P>
+                    </S.Content>
+                </S.Project>
+                <S.Project>
+                    <S.Img src={DogAPI} alt='Portflow' />
+                    <S.Content>
+                        <S.H2>DogAPI</S.H2>
+                        <S.P><S.A href="https://dog-api-dwmedeiros.netlify.app/" target="_blank" rel="noreferrer">Site</S.A></S.P>
+                        <S.P><S.A href="https://github.com/dwmedeirosDev/Dog-API" target="_blank" rel="noreferrer">Repositório GitHub</S.A></S.P>
+                    </S.Content>
+                </S.Project>
+                <S.Project>
+                    <S.Img src={Todolist} alt='Portflow' />
+                    <S.Content>
+                        <S.H2>To Do List</S.H2>
+                        <S.P><S.A href="https://to-do-list-dwmedeiros.netlify.app/" target="_blank" rel="noreferrer">Site</S.A></S.P>
+                        <S.P><S.A href="https://github.com/dwmedeirosDev/React-Challenge-08" target="_blank" rel="noreferrer">Repositório GitHub</S.A></S.P>
+                    </S.Content>
+                </S.Project>
+            </S.All>
+            <Footer />
+        </S.ContainerProjects>
     )
 }
--- src/Components/Projects/Style.js
+++ src/Components/Projects/Style.js
@@ -1,1 +1,59 @@
 import styled from 'styled-components'
+import Background from '../../Assets/Background/background.jpg'
+
+export const ContainerProjects = styled.section`
+    background-image: url(${Background});
+    font-family: 'Ubuntu', sans-serif;
+    height: 145.5vh;
+`
+
+export const All = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 40px;
+    margin-bottom: 40px;
+`
+
+export const Project = styled.div`
+    width: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    margin-left: 40px;
+`
+
+export const Img = styled.img`
+    width: 500px;
+    border-radius: 40px;
+    border: solid 1px white;
+`
+
+export const Content = styled.div`
+    color: white;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-left: 5%;
+`
+
+export const H2 = styled.h2`
+    font-size: 2rem;
+`
+
+export const P = styled.p`
+    margin-left: 40px;
+    border: solid 1px white;
+    padding: 10px;
+    font-weight: lighter;
+    transition: all 0.1s;
+
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const A = styled.a`
+    text-decoration: none;
+    color: white;
+`
